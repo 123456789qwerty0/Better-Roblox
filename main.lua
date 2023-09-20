050c89c5ed5863fae37f0c6539c69b72df38d117
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2023-09-20 15:27:13 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,3 @@
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game:GetService('Players').LocalPlayer ~= nil
+loadstring(game:HttpGet('https://eternityhub.xyz/BetterRoblox/Core/MainCore.lua'))()
