bf5edc64fb83049b14901d9bc628acba1de9daa7
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2023-09-20 20:22:31 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
 repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game:GetService('Players').LocalPlayer ~= nil
-loadstring(game:HttpGet('https://eternityhub.xyz/BetterRoblox/Core/MainCore.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/123456789qwerty0/Better-Roblox/main/core.txt'))()
